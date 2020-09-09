8026fcda0f8d3508b4a6f749ab0bca7035cb807c
hansputera <6bestfriendsramfah@gmail.com>
2020-09-09 04:08:09 +0000

    fix proxy bugs, and add proxy checker.

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import mongoClient from "./src/Mongo";
 const mongo = new mongoClient("Piring");
-(async() => {
+
+async function gabut() {
     await mongo.generator();
-})();
+}
+
+gabut();
--- src/ProxyHandle.ts
+++ src/ProxyHandle.ts
@@ -1,18 +1,27 @@
 import { ProxyOptions } from "./Interfaces";
 import request from "request";
 import { load } from "cheerio";
+import MongoError from "./MongoError";
+
+const urls: string[] = ["https://free-proxy-list.net/anonymous-proxy.html", "https://www.sslproxies.org", "https://free-proxy-list.net", "https://www.us-proxy.org"];
+
+
 
 export default class ProxyHandle {
+    public checkerCallback!: (proxy: string, success: boolean, statusCode: number, advanced: any) => void;
     constructor(public opt?: ProxyOptions) {
         if (!this.opt) {
+            const random: number = Math.floor(Math.random() * urls.length);
             this.opt = {
-                url: "https://www.sslproxies.org",
+                url: urls[random],
                 headers: {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
                 }
             }
         }
     }
+
+    
 
     async generate(): Promise<string> {
     let tempData: string[] = [];
@@ -28,8 +37,20 @@ export default class ProxyHandle {
             const port = ops.filter(x => x.length < 5);
             const num: number = Math.floor(Math.random() * port.length);
 
-            out += `https://${ip[num]}:${port[num]}`;
-            console.info("Using proxy: " + out);
+           
+            request({ url: "https://google.com", proxy: out, headers: { "User-Agent": this.opt?.headers?.["User-Agent"] }}, (err, res) => {
+                    console.info("[PROXY-ROTATOR] DO CHECKING PROXY!");
+                if (err) {
+                    throw new MongoError("Proxy Broken, Try Again!" + err);
+                } else if (res.statusCode != 200) {
+                    throw new MongoError("Proxy Broken, Code " + res.statusCode);
+                } else if (!res.body) {
+                    throw new MongoError("Proxy Broken, Can't find body!");
+                } else {
+                    out += `https://${ip[num]}:${port[num].length < 3 ? port[num] + '80' : port[num]}`;
+                    console.info("Using proxy: " + out);
+                }
+            });
         });
         return out;
     }
